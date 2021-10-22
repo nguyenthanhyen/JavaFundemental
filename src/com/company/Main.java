41ac0f05d20920e532119ff555a134f7db5a8f0b
nguyenthanhyen <thanhyen2906@gmail.com>
2021-10-22 19:26:37 +0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    /* Ghi chú:
+         - kiểu dữ liệu
+         - address = Quận 1 (Kiểu String)
+         - Kiểu số nguyên (int)
+         - float = số thực, số thập phân
+    */
+        String name = "Nguyễn Gia Hân";
+        int age = 12;
+        boolean isMale = true;
+        float height = 1.30f;
+
+        System.out.println("Tên: " + name + " Tuổi: " + age + " Chiều cao: " + height + " m" );
+        System.out.println("Is male" + true);
+
+    }
+}
